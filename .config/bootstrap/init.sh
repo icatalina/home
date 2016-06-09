100429c32847826705f892b702c1dc092ea0d3ed
Ignacio Catalina <icatalina@users.noreply.github.com>
2016-06-09 17:16:34 +0200

    Update init.sh

--- .config/bootstrap/init.sh
+++ .config/bootstrap/init.sh
@@ -1,12 +1,7 @@
 # install Xcode Command Line Tools
 # https://github.com/timsutton/osx-vm-templates/blob/ce8df8a7468faa7c5312444ece1b977c1b2f77a4/scripts/xcode-cli-tools.sh
 touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" -v;
+softwareupdate -i -a -v;
 
 git clone --bare git@github.com:icatalina/home.git $HOME/.cfg
 function config {
