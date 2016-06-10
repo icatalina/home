5a969a4ef52b2b654ee2c2910b4265851078acb6
Ignacio Catalina <icatalina@users.noreply.github.com>
2016-06-10 18:03:29 +0200

    Init&Update submod. and help message to continue

--- .config/bootstrap/init.sh
+++ .config/bootstrap/init.sh
@@ -17,3 +17,9 @@ if [ $? = 0 ]; then
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 config checkout
+
+config submodule init
+config submodule update
+
+echo "To continue installing the default apps run: ~/.config/bootstrap/systemsetup.sh"
+echo "To setup OSX run: ~/.config/bootstrap/osx.sh"
