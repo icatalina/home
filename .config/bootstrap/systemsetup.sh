443ca05d72678f9edff7c67ec1a70430b160bdd9
Ignacio Catalina <icc@Ignacios-MacBook-Pro.local>
2016-06-10 18:18:53 +0200

    Adding a few formulas and variables to paths

--- .config/bootstrap/systemsetup.sh
+++ .config/bootstrap/systemsetup.sh
@@ -1,87 +1,92 @@
 #!/bin/sh
 
+sudo sed '1s/^/\/usr\/local\/opt\/coreutils\/libexec\/gnubin\n\/usr\/local\/bin\n/' /etc/paths
+
 brewApps=(
-  coreutils
-  bash
-  cmake
-  caskroom/cask/brew-cask
-  diffutils
-  ed --default-names
-  file-formula
-  findutils --with-default-names
-  gawk
-  git
-  gnu-indent --with-default-names
-  gnu-sed --with-default-names
-  gnu-tar --with-default-names
-  gnu-which --with-default-names
-  gnutls
-  grep --with-default-names
-  gzip
-  less
-  python
-  python3
-  ngrok
-  node
-  rsync
-  ruby
-  screen
-  unzip
-  watch
-  wdiff --with-gettext
-  wget
-  tmux
-  the_silver_searcher
-  zsh
-  vim --override-system-vi --HEAD
-  reattach-to-user-namespace
-  neovim --HEAD
+  "coreutils"
+  "bash"
+  "cmake"
+  "caskroom/cask/brew-cask"
+  "diffutils"
+  "ed --default-names"
+  "file-formula"
+  "findutils --with-default-names"
+  "gawk"
+  "git"
+  "gnu-indent --with-default-names"
+  "gnu-sed --with-default-names"
+  "gnu-tar --with-default-names"
+  "gnu-which --with-default-names"
+  "gnutls"
+  "grep --with-default-names"
+  "gzip"
+  "jq"
+  "homebrew/binary/ngrok2"
+  "less"
+  "python"
+  "python3"
+  "node"
+  "rsync"
+  "ruby"
+  "screen"
+  "unzip"
+  "watch"
+  "wdiff --with-gettext"
+  "wget"
+  "tmux"
+  "the_silver_searcher"
+  "zsh"
+  "vim --with-override-system-vi"
+  "reattach-to-user-namespace"
+  "neovim/neovim/neovim"
 )
 
 casksApps=(
-  bettertouchtool
-  betterzipql
-  caffeine
-  caskroom/versions/firefoxdeveloperedition
-  caskroom/versions/google-chrome-canary
-  firefox
-  font-hack
-  google-chrome
-  imageoptim
-  iterm2
-  java
-  keka
-  skype
-  transmission
-  upm
-  viber
-  virtualbox
-  vlc
-  vox
+  "bettertouchtool"
+  "betterzipql"
+  "caffeine"
+  "caskroom/versions/firefoxdeveloperedition"
+  "caskroom/versions/google-chrome-canary"
+  "coteditor"
+  "firefox"
+  "filezilla"
+  "font-hack"
+  "google-chrome"
+  "imageoptim"
+  "iterm2"
+  "java"
+  "keka"
+  "skype"
+  "transmission"
+  "upm"
+  "viber"
+  "virtualbox"
+  "vlc"
+  "vox"
 )
 
 # QuickLook Plugins
 quickLookPlugins=(
-  qlcolorcode
-  qlimagesize
-  qlmarkdown
-  qlprettypatch
-  qlstephen
-  quicklook-csv
-  quicklook-json
-  webpquicklook
+  "qlcolorcode"
+  "qlimagesize"
+  "qlmarkdown"
+  "qlprettypatch"
+  "qlstephen"
+  "quicklook-csv"
+  "quicklook-json"
+  "webpquicklook"
 )
 
 nodePackages=(
-  csslint
-  eslint
-  grunt-cli
-  node-inspector
-  pure-prompt
+  "csslint"
+  "eslint"
+  "grunt-cli"
+  "node-inspector"
+  "pure-prompt"
 )
 
 gemPackages=(
-  scss_lint
+  "scss_lint"
 )
 
 # Check for Homebrew,
@@ -94,8 +99,9 @@ else
   echo "Brew already Installed..."
 fi
 
+echo "Taping a few things..."
 brew tap homebrew/versions
-brew install caskroom/cask/brew-cask
+brew tap caskroom/cask/brew-cask
 brew tap caskroom/fonts
 brew tap homebrew/dupes
 
@@ -103,55 +109,23 @@ brew tap homebrew/dupes
 echo "Updating Formulas..."
 brew update
 
-echo "Taping homebrew/dupes..."
-brew tap homebrew/dupes
-
 echo "Installing Stuff..."
 
 echo "Installing brew packages..."
-# brew install ${brewApps[@]}
-brew install coreutils
-brew install bash
-brew install caskroom/cask/brew-cask
-brew install diffutils
-brew install ed --default-names
-brew install file-formula
-brew install findutils --with-default-names
-brew install gawk
-brew install git
-brew install gnu-indent --with-default-names
-brew install gnu-sed --with-default-names
-brew install gnu-tar --with-default-names
-brew install gnu-which --with-default-names
-brew install gnutls
-brew install grep --with-default-names
-brew install gzip
-brew install less
-brew install python
-brew install python3
-brew install ngrok
-brew install node
-brew install rsync
-brew install ruby
-brew install screen
-brew install unzip
-brew install watch
-brew install wdiff --with-gettext
-brew install wget
-brew install tmux
-brew install the_silver_searcher
-brew install zsh
-brew install vim --override-system-vi --HEAD
+for (( i=0; i < ${#brewApps[@]}; i++ ))
+do
+  print "[%d/%d] Installing %s..." ((i+1)) "${#brewApps[@]}" "${brewApps[$i]}"
+  brew install ${brewApps[$i]}
+done
 
 echo "Installing Casks..."
 brew cask install ${casksApps[@]}
 
 echo "Installing QuickLookPlugins..."
-rew cask install ${quickLookPlugins[@]}
+brew cask install ${quickLookPlugins[@]}
 
 echo "Cleaning brew..."
 brew cleanup
-
 
 echo "Installing some node packages..."
 npm -g install ${nodePackages[@]}
